b112f7a549289a6999395df55f5bd47f38d5b136
Travis Vander Hoop <vanderhoop@me.com>
2019-03-20 10:57:45 -0400

    Render ideas on GroupingStage

--- web/static/js/components/grouping_stage.jsx
+++ web/static/js/components/grouping_stage.jsx
@@ -1,8 +1,10 @@
 import React from "react"
-import { Preview } from "react-dnd-multi-backend"
+import { Preview as TouchEventDragPreview } from "react-dnd-multi-backend"
 
 import LowerThird from "./lower_third"
+import GroupingStageIdeaCard from "./grouping_stage_idea_card"
 
+import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/idea_generation_stage.css"
 
 // specification for rendering a drag preview on touch devices
@@ -12,12 +14,24 @@ const generateTouchEventDragPreview = (type, item, defaultDraggingStyles) => {
   return <div style={style}>Drag Preview</div>
 }
 
-export default props => {
+const GroupingStage = props => {
+  const { ideas } = props
+
   return (
     <div className={styles.wrapper}>
-      <div style={{ flex: 1 }} />
+      <div style={{ flex: 1 }}>
+        {ideas.map(idea => (
+          <GroupingStageIdeaCard idea={idea} key={idea.id} />
+        ))}
+      </div>
       <LowerThird {...props} />
-      <Preview generator={generateTouchEventDragPreview} />
+      <TouchEventDragPreview generator={generateTouchEventDragPreview} />
     </div>
   )
 }
+
+GroupingStage.propTypes = {
+  ideas: AppPropTypes.ideas.isRequired,
+}
+
+export default GroupingStage
