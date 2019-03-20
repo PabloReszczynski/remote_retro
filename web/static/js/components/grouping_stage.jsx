0fee1cc0f02213197406c8097965fb8ac1238c5d
Travis Vander Hoop <vanderhoop@me.com>
2019-03-20 12:49:43 -0400

    Refactor: push droppable scope down into GroupingBoard component

--- web/static/js/components/grouping_stage.jsx
+++ web/static/js/components/grouping_stage.jsx
@@ -1,36 +1,26 @@
 import React from "react"
-import { Preview as TouchEventDragPreview } from "react-dnd-multi-backend"
+import PropTypes from "prop-types"
 
 import LowerThird from "./lower_third"
-import GroupingStageIdeaCard from "./grouping_stage_idea_card"
+import GroupingBoard from "./grouping_board"
 
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/idea_generation_stage.css"
 
-// specification for rendering a drag preview on touch devices
-// https://github.com/LouisBrunner/dnd-multi-backend/tree/master/packages/react-dnd-multi-backend#preview
-const generateTouchEventDragPreview = (type, item, styles) => {
-  return <GroupingStageIdeaCard touchEventDragPreviewStyles={styles} idea={item.draggedIdea} />
-}
-
 const GroupingStage = props => {
-  const { ideas } = props
+  const { ideas, actions } = props
 
   return (
     <div className={styles.wrapper}>
-      <div style={{ flex: 1 }}>
-        {ideas.map(idea => (
-          <GroupingStageIdeaCard idea={idea} key={idea.id} />
-        ))}
-      </div>
+      <GroupingBoard ideas={ideas} actions={actions} />
       <LowerThird {...props} />
-      <TouchEventDragPreview generator={generateTouchEventDragPreview} />
     </div>
   )
 }
 
 GroupingStage.propTypes = {
   ideas: AppPropTypes.ideas.isRequired,
+  actions: PropTypes.object.isRequired,
 }
 
 export default GroupingStage
